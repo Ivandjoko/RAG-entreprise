830456aed86f87b2530774edbc0e22b55bec98f1
Ivandjoko <kamdemdjokoivan@gmail.com>
2026-08-06 00:02:00 +0200

    .

--- infrastructure/bootstrap/main.tf
+++ infrastructure/bootstrap/main.tf
@@ -92,5 +92,3 @@ resource "aws_iam_role_policy_attachment" "deployer" {
   role       = aws_iam_role.github_actions.name
   policy_arn = aws_iam_policy.terraform_deployer.arn
 }
-
-AWS_PROFILE=AdministratorAccess-Bootstrap-027457927285 terraform plan -state=terraform.dev.tfstate -var="github_org=Ivandjoko" -var="environment_suffix=dev"
